ad02221ad034353f9953bdede1337096b9eb0e20
Bob Chen <chenbobby168@gmail.com>
2020-07-25 10:52:09 +0000

    Client reads stdin and sends to server.

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,32 @@
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::os::unix::net::UnixStream;
+use std::io;
 use std::io::prelude::*;
 
 const UNIX_SOCKET_PATH: &str = "/tmp/bongodb.sock";
 
 fn main() -> std::io::Result<()> {
+    let stdin_reader = BufReader::new(io::stdin());
     let mut stream = UnixStream::connect(UNIX_SOCKET_PATH)?;
-    stream.write_all(b"Hello, BongoDB.\n")?;
     let mut buffer = [0; 64];
-    stream.read(&mut buffer)?;
-    let data = String::from_utf8(buffer.to_vec()).unwrap();
-    println!("{}", &data);
+
+    for line in stdin_reader.lines() {
+        match line {
+            Ok(line) => {
+                println!("Read: {:?}", line);
+
+                writeln!(stream, "{}", line).unwrap();
+                println!("Written.");
+
+                stream.read(&mut buffer)?;
+                let data = String::from_utf8(buffer.to_vec()).unwrap();
+                println!("Received: {}", &data);
+            },
+            Err(err) => {
+                println!("{}", err);
+                break;
+            },
+        }
+    }
     Ok(())
 }
